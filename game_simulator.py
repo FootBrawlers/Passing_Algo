23a6a05c2774baf8729cf77186a80e59d0b656ab
nobi1007 <mittalshyam1007@gmail.com>
2020-01-09 19:01:49 +0530

    added boundaries of field

--- game_simulator.py
+++ game_simulator.py
@@ -15,15 +15,18 @@ field_length = 900
 field_width = 600 
 center_x = 570
 center_y = 310
-
+circle_radius = 80
+goal_post_length = (2*circle_radius)-20
+width_small_d = 50
+width_big_d = 150
 
 # STYLING
 screen_color = (0,0,0)
-field_color = (4,204,130)
+field_color = (4,255,100)
 field_border_color = (255,255,255)
-host_bot_color = (0,0,255)
+host_bot_color = (255,255,0)
 opp_bot_color = (255,0,0)
-
+goal_post_color = (101,67,33)
 
 host_cord = {}
 opp_cord = {}
@@ -51,10 +54,32 @@ while True:
     pygame.draw.line(screen, field_border_color, (center_x,field_y), (center_x, field_y + field_width),1)
 
     # center circle
-    pygame.draw.circle(screen, field_border_color, (center_x,310), 50,1)
+    pygame.draw.circle(screen, field_border_color, (center_x,310), circle_radius,1)
 
     # center line horizontal
-    pygame.draw.line(screen, field_border_color, (field_x,center_y), (field_x + field_length, center_y),1)
+    # pygame.draw.line(screen, field_border_color, (field_x,center_y), (field_x + field_length, center_y),1)
+
+
+    # LEFT Side
+
+    # goalpost
+    pygame.draw.line(screen, goal_post_color, (field_x-20,center_y-(goal_post_length//2)), (field_x-20, center_y+(goal_post_length//2)),19)
+    # smallD
+    pygame.draw.rect(screen, field_border_color, Rect((field_x-1,center_y-(circle_radius)), (width_small_d,2*circle_radius)),1)
+    # bigD
+    pygame.draw.rect(screen, field_border_color, Rect((field_x-1,center_y-((5/8) * (field_width//2))), (width_big_d,(10/8)*(field_width//2))),1)
+
+
+
+    # RIGHT side
+
+    # goalpost
+    pygame.draw.line(screen, goal_post_color, (field_x +field_length+19,center_y-(goal_post_length//2)), (field_x +field_length+19, center_y+(goal_post_length//2)),19)
+    # smallD
+    pygame.draw.rect(screen, field_border_color, Rect((field_x + field_length -(width_small_d - 1),center_y-(circle_radius)), (width_small_d,2*circle_radius)),1)
+    # bigD
+    pygame.draw.rect(screen, field_border_color, Rect((field_x + field_length -(width_big_d - 1),center_y-((5/8) * (field_width//2))), (width_big_d,(10/8)*(field_width//2))),1)
+
 
     screen.unlock()
     if event.type == pygame.MOUSEBUTTONDOWN and len(host_cord)<7:
