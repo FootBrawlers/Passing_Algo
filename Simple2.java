2a28b7041f25e6aa8ac7748a1761a68cd3cf8b90
TejaswiKarasani <tejaswikarasani@gmail.com>
2020-01-15 18:47:49 +0530

    removing all the commented code

--- Simple2.java
+++ Simple2.java
@@ -7,7 +7,7 @@ class Simple2
 	//checking whether (x'',y'') is present inside the ellipse or not
 	
 	//checking whether there is any obstacle is present in the elipse or not
-	static boolean checkrectangle(int i_x,int i_y,int f_x,int f_y,int ob_x,int ob_y,int l,int ci)
+	static boolean checkrectangle(int i_x,int i_y,int f_x,int f_y,int ob_x,int ob_y,int l)
 	{
 		
 		double p=Math.sqrt(((i_x-f_x)*(i_x-f_x))+((i_y-f_y)*(i_y-f_y))); //getting the "b" in the ellipse 
@@ -62,12 +62,9 @@ class Simple2
 					everything[2]=Integer.parseInt(temp[2]);
 					read_list.add(everything);
 					
-					//System.out.println(st[i]); 
+					
 				}
-			// for(int x:read_list.get(i))
-			// {
-			// 	//System.out.print(x+" "); //]+" "+read_list.get(i)[1]+" "+read_list.get(i)[2]);
-			// }
+			
 			}
 			
 			for(int i=0;i<12;i++)
@@ -76,7 +73,7 @@ class Simple2
 				get_x[i]=read_list.get(i)[1];
 				get_y[i]=read_list.get(i)[2];
 			}
-			//System.out.println();
+			
 		}
 		catch (IOException e) {
 
@@ -95,7 +92,6 @@ class Simple2
 		} 
 		
 		
-		//System.out.println(read_list);
 		
 		
 		// --------------------------------- VERIFYING THE INPUT ---------------------------------------
@@ -107,46 +103,26 @@ class Simple2
 		
 		
 		 
-		final int width=700,length=900; //dimensions of football
+		final int width=700,length=900; //dimensions of football ground
 		int nearest_goal_x=0,nearest_goal_y=0; //checking for the point which is nearest to goal post so this becomes the one having ball then it can goal directly
-		//int x1[]=new int[12];
-		//int y1[]=new int[12];
+	
 		int x2[]=new int[6];
 		int y2[]=new int[6];
 		int x3[]=new int[6];
 		int y3[]=new int[6];
 		int count=0;
 		int g1_x=0,g2_x=length-1,g1_y=(width-1)/2,g2_y=(width-1)/2,imagine_width=40; //imagine_width is nothing but l1 in the ellipse that used in checkrectangle function
-		//count_iteration=0;
+		
 		
 		List<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
 		List<ArrayList<Integer>> matched_list = new ArrayList<ArrayList<Integer>>();
 		List<ArrayList<Integer>> final_list=new ArrayList<ArrayList<Integer>>();
 		
-		/*Scanner sc = new Scanner(System.in);
-		for(int i=0;i<12;i++)
-		{
-        Integer a=sc.nextInt();
-        Integer b=sc.nextInt();
-        ArrayList<Integer> temp = new ArrayList<Integer>();
-        temp.add(a);
-        temp.add(b);
-        list.add(temp);
-		}
 		
-		for (int i = 0; i < list.size(); i++)
-		{ 
-					 
-				x1[i]=list.get(i).get(0);
-				y1[i]=list.get(i).get(1);	
-				System.out.println(x1[i] + "," + y1[i]);
-		
-   
-        } */
-		//3 2 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 11 11 12 12
-		//1 1 1 2 3 4 4 5 5 6 5 5 6 7 6 8 7 9 8 10 9 10 11 11
 		double len_between_goal=900;
+		
 		//logic to find the one which is nearest to the goal post so can goal instead of passing
+		
 		for(int i=0;i<6;i++)
         {
 				if((Math.sqrt(((g2_x-get_x[i])*(g2_x-get_y[i]))+((g2_y-get_y[i])*(g2_y-get_y[i])))) < len_between_goal)
@@ -154,18 +130,11 @@ class Simple2
 					nearest_goal_x=get_x[i];
 					nearest_goal_y=get_y[i];
 					len_between_goal=Math.sqrt(((g2_x-get_x[i])*(g2_x-get_y[i]))+((g2_y-get_y[i])*(g2_y-get_y[i])));
-					//new_id=id_list2.get(i);
-					//pole_dis2_count++;
-					//System.out.println("Coordinates nearest to the goal are :" + nearest_goal_x + nearest_goal_y);
-					System.out.println(nearest_goal_x);
-					System.out.println(nearest_goal_y);
-					System.out.println(len_between_goal);
 				}
 
-			//System.out.println("Coordinates nearest to the goal are :" + nearest_goal_x + nearest_goal_y);
 		}
 		
-		//System.out.println("Coordinates nearest to the goal are :" + nearest_goal_x + nearest_goal_y);
+		System.out.println("Coordinates nearest to the goal are :" + "(" +  nearest_goal_x + "," + nearest_goal_y + ")");
 		
 		int current_x=get_x[0],current_y=get_y[0]; //setting the one having ball as our first coordinate in the file
 		
@@ -176,12 +145,11 @@ class Simple2
 			
 				if(Math.sqrt(((current_x-get_x[i])*(current_x-get_x[i]))+((current_y-get_y[i])*(current_y-get_y[i])))<=650)
 				{
-					//System.out.println("Values are *****");
-					//System.out.println("(" + current_x + "," + current_y + ")" + "," + "(" + x1[i] + "," + y1[i] + ")");
+					
 					ArrayList<Integer> matched_temp = new ArrayList<Integer>();
 					matched_temp.add(get_x[i]);
 					matched_temp.add(get_y[i]);
-					//System.out.println(x1[i]+" "+y1[i] + " "+ matched_temp);
+					
 					
 					matched_list.add(matched_temp);
 					id.add(id_array1[i]);
@@ -197,7 +165,7 @@ class Simple2
 					 
 				x2[i] = matched_list.get(i).get(0);
 				y2[i] = matched_list.get(i).get(1);	
-				//System.out.println(x2[i] + "," + y2[i]);
+				
    
         }
         
@@ -210,27 +178,24 @@ class Simple2
         int k=0;
         while(k<matched_list.size())
         { 
-			int count_of_non_obstacles=0,c=0;
+			int count_of_non_obstacles=0;
 			for(int i=6;i<12;i++)
 			{
-				//checkrectangle(current_x,current_y,x2[k],y2[k],get_x[i],get_y[i],len_ob);
-				if(checkrectangle(current_x,current_y,x2[k],y2[k],get_x[i],get_y[i],imagine_width,c))
-				//if(get_x[i]>x2[k] && get_x[i]>current_x && get_y[i]>y2[k] && get_y[i]>current_y)
+		
+				if(checkrectangle(current_x,current_y,x2[k],y2[k],get_x[i],get_y[i],imagine_width))
+				
 				{
 					count_of_non_obstacles++;		
 				}
 				if(i==11 && count_of_non_obstacles==6)
 				{	
-					//id_array2[c]=id_array[k];
+					
 					id_list2.add(id.get(k));
 					ArrayList<Integer> final_temp = new ArrayList<Integer>();
 					final_temp.add(x2[k]);
 					final_temp.add(y2[k]);
 					final_list.add(final_temp);
-					//c++;
-					//System.out.println(x2[k] + "," + y2[k]);
 				}
-				c++;
 			}
 			System.out.println("No. of non obstacles are " + count_of_non_obstacles);
 			k++;
@@ -241,9 +206,11 @@ class Simple2
 					 
 				x3[i]=final_list.get(i).get(0);
 				y3[i]=final_list.get(i).get(1);
-				System.out.println("Final list values are: ");	
-				System.out.println(x3[i] + "," + y3[i]);
+				//System.out.println("Final list values are: ");	
+				//System.out.println(x3[i] + "," + y3[i]);
         }
+        
+        System.out.println("Final list values are " + final_list);
        int new_x=0,new_y=0,new_id=0; //new_x,new_y,new_id for storing the id and coordinates of our final condition satisfying one
        
        
@@ -313,32 +280,7 @@ class Simple2
 		}
         
 		
-		//System.out.println("-----------------");
-		//System.out.println("Count of matching coordinates within 650 cm distance  :  " + count);
-		//System.out.println("-----------------");
 		
-		//Testing our output
-		
-		/*System.out.println("After first condition id's stasifying are:");
-		for(int i=0;i<id.size();i++)
-			System.out.println("id = " + id);
-			
-		System.out.println("After second condition id's stasifying are:");
-		for(int i=0;i<id_list2.size();i++)
-			System.out.println("id = " + id);*/
-
-		/*try {
-            FileWriter writer = new FileWriter("output.txt");
-            BufferedWriter bwr = new BufferedWriter(writer);
-            bwr.write("");
-            bwr.write(" ");
-            bwr.write("Hobert");
-            bwr.close();
-            System.out.println("succesfully written to a file");
-            
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }*/
  
 	}
 
